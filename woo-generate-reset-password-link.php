94a0201396b9b2ef0887823d2b20d0f3500f8b00
Andrew Minion <andrew@andrewrminion.com>
2016-06-23 15:04:11 -0400

    set up function to return password reset link

--- woo-generate-reset-password-link.php
+++ woo-generate-reset-password-link.php
@@ -12,3 +12,14 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
+// generate password reset link
+function woo_new_account_password_reset_link( $user_login ) {
+    $user_data = get_user_by('login', $user_login);
+
+    // Redefining user_login ensures we return the right case in the email
+    $user_login = $user_data->user_login;
+    $user_email = $user_data->user_email;
+    $key = get_password_reset_key( $user_data );
+
+    echo network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login');
+}
