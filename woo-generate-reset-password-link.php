a1590a14a603c607c5e8b6f1ab362dd2448684ab
Andrew Minion <andrew@andrewrminion.com>
2016-06-23 15:38:35 -0400

    use plugin-supplied email templates

--- woo-generate-reset-password-link.php
+++ woo-generate-reset-password-link.php
@@ -23,3 +23,36 @@ function woo_new_account_password_reset_link( $user_login ) {
 
     echo network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login');
 }
+
+// use plugin-supplied template
+add_filter( 'woocommerce_locate_template', 'woo_new_account_password_locate_templates', 10, 3 );
+function woo_new_account_password_locate_templates( $template, $template_name, $template_path ) {
+    global $woocommerce;
+
+    $old_template = $template;
+    if ( ! $template_path ) {
+        $template_path = $woocommerce->template_url;
+    }
+    $plugin_path  = plugin_dir_path( __FILE__ ) . 'woocommerce/';
+
+    // Look within passed path within the theme - this is priority
+    $template = locate_template(
+        array(
+            $template_path . $template_name,
+            $template_name,
+        )
+    );
+
+    // Modification: Get the template from this plugin, if it exists
+    if ( ! $template && file_exists( $plugin_path . $template_name ) ) {
+        $template = $plugin_path . $template_name;
+    }
+
+    // Use default template
+    if ( ! $template ) {
+        $template = $old_template;
+    }
+
+    // Return what we found
+    return $template;
+}
