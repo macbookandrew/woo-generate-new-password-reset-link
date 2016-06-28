3fb49a08e52d647e2df330a1e0139bd3172e33e5
Andrew Minion <andrew@andrewrminion.com>
2016-06-28 09:34:18 -0400

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function( grunt ) {
+
+    'use strict';
+    var banner = '/**\n * <%= pkg.homepage %>\n * Copyright (c) <%= grunt.template.today("yyyy") %>\n * This file is generated automatically. Do not edit.\n */\n';
+    // Project configuration
+    grunt.initConfig( {
+
+        pkg:    grunt.file.readJSON( 'package.json' ),
+
+        addtextdomain: {
+            options: {
+                textdomain: 'woo-generate-new-password-reset-link',
+            },
+            target: {
+                files: {
+                    src: [ '*.php', '**/*.php', '!node_modules/**', '!php-tests/**', '!bin/**' ]
+                }
+            }
+        },
+
+        wp_readme_to_markdown: {
+            your_target: {
+                files: {
+                    'README.md': 'readme.txt'
+                }
+            },
+            options: {
+                screenshot_url: 'http://ps.w.org/woo-generate-new-password-reset-link/assets/{screenshot}.png',
+            }
+        },
+
+        makepot: {
+            target: {
+                options: {
+                    domainPath: '/languages',
+                    mainFile: 'woo-generate-new-password-reset-link.php',
+                    potFilename: 'woo-generate-new-password-reset-link.pot',
+                    potHeaders: {
+                        poedit: true,
+                        'x-poedit-keywordslist': true
+                    },
+                    type: 'wp-plugin',
+                    updateTimestamp: true
+                }
+            }
+        },
+    } );
+
+    grunt.loadNpmTasks( 'grunt-wp-i18n' );
+    grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+    grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
+    grunt.registerTask( 'readme', ['wp_readme_to_markdown']);
+
+    grunt.util.linefeed = '\n';
+
+};
