6a50c65eb9936037931b94a76ec9509c5076268b
Andrew Minion <andrew@andrewrminion.com>
2018-12-29 14:43:23 -0500

    update author name

--- woo-generate-new-password-reset-link.php
+++ woo-generate-new-password-reset-link.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://andrewrminion.com/2016/06/woo-generate-new-password-reset-link/
  * Description: Sends new account users an email with a password reset link rather than an auto-generated password
  * Version: 1.0.0
- * Author: AndrewRMinion Design
+ * Author: Andrew Minion
  * Author URI: https://www.andrewrminion.com
  */
 
